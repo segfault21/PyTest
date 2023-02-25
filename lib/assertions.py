87013ffc40e9cc31662c1d624e9abc89ed526649
segfault21 <ustalov161@gmail.com>
2023-02-25 15:52:12 +0300

    done

---
+++ lib/assertions.py
@@ -0,0 +1,15 @@
+from requests import Response
+import json
+
+
+
+class Assertions:
+    @staticmethod
+    def assert_json_value_by_name(response: Response, name, expected_value, error_message):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON, is {response.text}"
+
+        assert name in response_as_dict, f"Response JSON doesnt have {name}"
+        assert response_as_dict[name] == expected_value, error_message
